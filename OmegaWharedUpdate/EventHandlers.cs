0e9384b8878b0474cfc4260ffa234b540262e1c4
An4r3w <75725607+An4r3w@users.noreply.github.com>
2021-03-09 15:50:53 +0100

    Add files via upload

---
+++ OmegaWharedUpdate/EventHandlers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using Exiled.API.Features;
+using Exiled.Events.EventArgs;
+
+namespace OmegaWharedUpdate
+{
+    public class EventHandlers
+    {
+		public void onWarheadExplosion()
+		{
+			foreach (var player in Player.List)
+			{
+				player.Kill(DamageTypes.Nuke);
+				player.Broadcast((ushort)Plugin.Singleton.Config.BroadcastTime, Plugin.Singleton.Config.DetonationBroadcast, 0);
+				Cassie.Message(Plugin.Singleton.Config.CassieMessage);
+			}
+		}
+    }
+}
---
+++ OmegaWharedUpdate/Plugin.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Exiled.API.Enums;
+using Exiled.API.Features;
+using WarheadEvent = Exiled.Events.Handlers.Warhead;
+
+namespace OmegaWharedUpdate
+{
+    internal class Plugin : Plugin<Config>
+    {
+        public override string Author { get; } = "An4r3w";
+
+        public override string Name { get; } = "Omega Warhead";
+
+        public override string Prefix { get; } = "Omega Warhead";
+
+        public override Version Version { get; } = new Version(1, 0, 0);
+
+        public override Version RequiredExiledVersion { get; } = new Version(2, 8, 0);
+
+        internal EventHandlers EventHandlers { get; set; }
+
+        public override PluginPriority Priority { get; } = PluginPriority.Default;
+
+        public override void OnEnabled()
+        {
+            Plugin.Singleton = this;
+            this.EventHandlers = new EventHandlers();
+            WarheadEvent.Detonated += EventHandlers.onWarheadExplosion;
+        }
+
+        public override void OnDisabled()
+        {
+            WarheadEvent.Detonated -= EventHandlers.onWarheadExplosion;
+            this.EventHandlers = null;
+        }
+
+
+        public static Plugin Singleton;
+    }
+}
